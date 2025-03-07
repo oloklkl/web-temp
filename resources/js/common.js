01d3b92917f61cf21b62f249d35ed94b98638206
oloklkl <sungju7694@naver.com>
2025-03-07 17:12:50 +0900

    기초 작업 진짜 완료

--- resources/js/common.js
+++ resources/js/common.js
@@ -1,5 +1,42 @@
-// 전체 공통
+const get = (target) => document.querySelector(target);
+const getAll = (target) => document.querySelectorAll(target);
 
-// 여기에서 경로 수정
-const headerFilePath = '/page/layout/header.html';
-const footerFilePath = '/page/layout/footer.html';
+// a 태그의 기본 이벤트 방지
+const preventDefaultAnchor = () => {
+    const $links = getAll('a[href="#"]');
+    $links.forEach((link) => {
+        if (!link.classList.contains('noPrevent')) {
+            link.addEventListener('click', (e) => e.preventDefault());
+        }
+    });
+};
+
+// 네비게이션 메뉴 이벤트 핸들링
+const navBar = () => {
+    const $gnblis = getAll('#gnb > ul > li');
+    const $gnbuls = getAll('#gnb ul ul'); // 모든 서브메뉴
+    const $gnb = get('#gnb');
+
+    $gnblis.forEach((li) => {
+        li.addEventListener('mouseenter', () => {
+            // 기존 활성화된 on 클래스 제거
+            $gnbuls.forEach((ul) => ul.classList.remove('on'));
+
+            // 현재 li의 하위 ul만 on 추가
+            const currentUl = li.querySelector('ul');
+            if (currentUl) {
+                currentUl.classList.add('on');
+            }
+        });
+    });
+
+    $gnb.addEventListener('mouseleave', () => {
+        $gnbuls.forEach((ul) => ul.classList.remove('on'));
+    });
+};
+
+// 초기 실행
+(() => {
+    preventDefaultAnchor();
+    navBar();
+})();
